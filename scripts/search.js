6fd1a76220f39cf1147610a11562de056acab27a
Gena Jey <thipi1213@live.com>
2024-11-20 17:09:43 -0500

    Add some styling and fix the search

--- scripts/search.js
+++ scripts/search.js
@@ -1,29 +1,31 @@
-function search(event) {
+function search(keywordsArray, genre, year) {
     // Returns and array of movies that should appear as the result of search
 
-    let keywordsArray = event.target.keyword.value.split();
-    let genre = event.target.genre.value;
-    let year = event.target.genre.value;
+    console.log('enteres into serahc function')
+
     let searchResult = [];
-
+    
     for (j = 0; j < moviesData.length; j++) {
-
         let movieObj = moviesData[j];
+
         let movieInSearchResult = false;
 
         for (i = 0; !movieInSearchResult && i <keywordsArray.length; i++){
 
-            if (moviesObj[Title].includes(keywordsArray[i])){
-                searchResult.push(moviesObj);
+            if (movieObj.Title.includes(keywordsArray[i])){
+                console.log('obj title is', movieObj.Title)
+                searchResult.push(movieObj);
                 movieInSearchResult = true;
             } 
         }
 
-        if (!movieInSearchResult && movieObj[Genre].includes(genre)) {
+        if (!movieInSearchResult && movieObj.Genre.includes(genre)) {
+            console.log('movie genre is ', movieObj.Genre, typeof movieObj.Genre)
             searchResult.push(movieObj);
             movieInSearchResult = true;
         } else if (!movieInSearchResult && movieObj.Year === year){
-            searchResult.push(moviesObj);
+            console.log(movieObj.year, 'yera is');
+            searchResult.push(movieObj);
         }
     }
 
@@ -35,7 +37,7 @@ function createMovieCard(item){
     cardEL.classList.add('card');
 
     const imgEl = document.createElement('img');
-    imgEl.src = item[Poster];
+    imgEl.src = item.Poster;
     cardEL.classList.add('card__poster');
     cardEL.appendChild(imgEl);
 
@@ -46,7 +48,7 @@ function createMovieCard(item){
 
 
     const directorEL = document.createElement('p');
-    directorEl.textContent = item.Director.split(',')[0];
+    directorEL.textContent = item.Director.split(',')[0];
     directorEL.classList.add('card__director')
     cardEL.appendChild(directorEL);
 
@@ -54,22 +56,42 @@ function createMovieCard(item){
     ratingEl.textContent = 'IMDB rating:' + item.imdbRating;
     ratingEl.classList.add('card__rating');
     cardEL.appendChild(ratingEl);
+
+    return cardEL
   
+}
+
+function renderMovies(searchResultsArray) {
+    const resultsEl = document.getElementById('results');
+
+    resultsEl.innerHTML = "";
+
+
+
+    searchResultsArray.forEach((movie)=> {
+        const movieCard = createMovieCard(movie);
+        resultsEl.appendChild(movieCard);
+    })
 }
 
 
 
 function handleSubmit(event) {
+
+    console.log('form did submit')
+
     event.preventDefault();
 
-    const results = document.querySelector();
+    const keywordsArray = event.target.keyword.value.split();
+    let genre = event.target.genre.value;
+    let year = event.target.genre.value;
 
-    const searchResultsArray = search(event);
+    const searchResultsArray = search(keywordsArray, genre, year);
+    console.log('search results are', searchResultsArray[0])
 
-    searchResultsArray.forEach((movie)=> {
-        const movieCard = createMovieCard(movie);
-        results.appendChild(movieCard);
-    })
-
-
+    renderMovies(searchResultsArray)
 }
+
+const formEl = document.getElementById('search');
+
+formEl.addEventListener('submit', handleSubmit);
