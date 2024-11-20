aa83a828b22528002e0ac2ab1626d78514be32e0
Gena Jey <thipi1213@live.com>
2024-11-20 16:00:28 -0500

    Added css and html code to mine

--- scripts/search.js
+++ scripts/search.js
@@ -30,8 +30,31 @@ function search(event) {
     return searchResult
 }
 
-function movieCard(item){
+function createMovieCard(item){
+    const cardEL = document.createElement('section');
+    cardEL.classList.add('card');
 
+    const imgEl = document.createElement('img');
+    imgEl.src = item[Poster];
+    cardEL.classList.add('card__poster');
+    cardEL.appendChild(imgEl);
+
+    const titleEl = document.createElement('p');
+    titleEl.textContent = item.Title;
+    titleEl.classList.add('card__title');
+    cardEL.appendChild(titleEl);
+
+
+    const directorEL = document.createElement('p');
+    directorEl.textContent = item.Director.split(',')[0];
+    directorEL.classList.add('card__director')
+    cardEL.appendChild(directorEL);
+
+    const ratingEl = document.createElement('p');
+    ratingEl.textContent = 'IMDB rating:' + item.imdbRating;
+    ratingEl.classList.add('card__rating');
+    cardEL.appendChild(ratingEl);
+  
 }
 
 
@@ -39,10 +62,13 @@ function movieCard(item){
 function handleSubmit(event) {
     event.preventDefault();
 
+    const results = document.querySelector();
+
     const searchResultsArray = search(event);
 
     searchResultsArray.forEach((movie)=> {
-        movieCard(movie);
+        const movieCard = createMovieCard(movie);
+        results.appendChild(movieCard);
     })
 
 
